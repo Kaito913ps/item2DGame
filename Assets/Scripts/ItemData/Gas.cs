278388c509d51c823b56c5065b88af64b26ec08d
DESKTOP-TRSHQ94\kaito <kaito913jp@gmail.com>
2023-02-24 14:36:32 +0900

    コミット

---
+++ Assets/Scripts/ItemData/Gas.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class Gas : MonoBehaviour
+{
+    [SerializeField] float _defaultGravity;
+
+    SpriteRenderer _spriteRenderer;
+    Rigidbody2D _rigidbody2D;
+    [SerializeField] int _heavy = 0;
+    int _point = 0;
+    float _bourst = 0;
+    Vector2 _velocity = Vector2.zero;
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _velocity = Vector2.zero;
+        _bourst = 1 + ((float)_heavy / 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _velocity.y += (_defaultGravity * _bourst) * Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.velocity = _velocity;
+    }
+}
